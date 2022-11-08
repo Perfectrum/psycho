03adc42f3baa8f8fc0dedb219e96ab89c629f5ba
Lsr248 <i-fattakhova@mail.ru>
2022-11-08 17:31:08 +0300

    main page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,29 @@
+import "./main.css"
 
 export function Main() {
-    return (<div>Main page</div>);
+    return ( 
+    <div className="flex-container">
+        <div id="left_menu">
+             <div  className="items" > 
+                <a href="#" className="active">Входящие</a>
+                <a href="#">Цели 1</a>
+                <a href="#">Задачи 2</a>
+        </div>
+        
+        </div>
+        <div id="center">
+        <ul>
+            <li><a className="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+        
+        
+        </div>
+        <div id="right_bar">
+            <button id="avatar" className="user"></button>
+        </div>
+</div>
+);
 }
