a9964eb57f4cefaefe618130c7ccb0cce98059ee
Semyon <abakumovs882@gmail.com>
2022-11-08 16:41:46 +0300

    develop
    fix static and media

--- backend/psycho_back/urls.py
+++ backend/psycho_back/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -28,3 +30,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/register/", RegisterView.as_view(), name="register"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
