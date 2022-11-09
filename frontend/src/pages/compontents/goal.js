3d0dcc11220738d42b52aa1040423560cbe5a942
Lsr248 <i-fattakhova@mail.ru>
2022-11-10 01:33:09 +0300

    goals

---
+++ frontend/src/pages/compontents/goal.js
@@ -0,0 +1,27 @@
+import './goal.css';
+
+export function Goal(props) {
+    const { content } = props;
+    const { name, desc } = content;
+
+    return (
+        <div className="goal-card">
+            <div className='goal-card-right'>
+                <img src="star.png"/>
+                {name}
+            </div>
+            <div className="goal-center-card">
+                <div className="goal-desc">
+                    {desc} 
+                </div>
+                <div className='edit-goal'> 
+                    <div className='edit-goal-tag'>
+                        <img src="./edit.png"></img>
+                        Редактировать
+                    </div>
+                </div>
+                
+            </div> 
+        </div>
+    );
+}
